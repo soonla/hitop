ca04e6e1af51f7a45ba8d543912368769aa84830
sunggyu <treelee96@naver.com>
2021-11-10 17:29:12 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const mainVisualSlider = new Swiper("#mainVisual", {
+  autoplay: true,
+  speed: 1000,
+  effect: "fade",
+  loop: true,
+  navigation: {
+    prevEl: "#mainVisual .prev",
+    nextEl: "#mainVisual ,next",
+  },
+});
+
+//f(x) = x+3;
